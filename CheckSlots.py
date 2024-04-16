f3dd0e32b310dedc7e7504b026175f2137d45253
Alex <blr.ymka@tut.by>
2024-04-16 21:16:01 +0300

    changes in check slot process - add random cursor moving - add random sleep timer

--- CheckSlots.py
+++ CheckSlots.py
@@ -19,7 +19,9 @@ class CheckSlots:
         icon = pyautogui.locateOnScreen('Images\\choose_center.PNG', 5, grayscale=True, confidence=0.8)
         time.sleep(animation['fast_duration'])
         pyautogui.moveTo(*pyautogui.center(icon), duration=animation['slow_duration'])
-        pyautogui.move(50, 50, duration=animation['middle_duration'])
+        pyautogui.click()
+        time.sleep(animation['fast_duration'])
+        pyautogui.move(random.randint(-50, 50), 50, duration=animation['middle_duration'])
         pyautogui.click()
 
         # scroll to option
@@ -40,7 +42,7 @@ class CheckSlots:
         icon = pyautogui.locateOnScreen('Images\\choose_category.PNG', 5, grayscale=True, confidence=0.8)
         time.sleep(animation['fast_duration'])
         pyautogui.moveTo(*pyautogui.center(icon), duration=animation['slow_duration'])
-        pyautogui.move(50, 50, duration=animation['middle_duration'])
+        pyautogui.move(random.randint(-50, 50), 50, duration=animation['middle_duration'])
         pyautogui.click()
 
         # pick category
@@ -58,7 +60,7 @@ class CheckSlots:
         icon = pyautogui.locateOnScreen('Images\\birthday.PNG', 5, grayscale=True, confidence=0.8)
         time.sleep(animation['fast_duration'])
         pyautogui.moveTo(*pyautogui.center(icon), duration=animation['slow_duration'])
-        pyautogui.move(50, 50, duration=animation['middle_duration'])
+        pyautogui.move(random.randint(-50, 50), 50, duration=animation['middle_duration'])
         pyautogui.tripleClick()
         pyautogui.press('delete')
         pyautogui.write(credentials["birthday"], 0.1)
@@ -73,7 +75,7 @@ class CheckSlots:
         icon = pyautogui.locateOnScreen('Images\\citizenship.PNG', 5, grayscale=True, confidence=0.8)
         time.sleep(animation['fast_duration'])
         pyautogui.moveTo(*pyautogui.center(icon), duration=animation['pre_middle_duration'])
-        pyautogui.move(50, 50, duration=animation['pre_middle_duration'])
+        pyautogui.move(random.randint(-50, 50), 50, duration=animation['pre_middle_duration'])
         pyautogui.click()
 
         # scroll to option
@@ -87,7 +89,7 @@ class CheckSlots:
         pyautogui.moveTo(*pyautogui.center(icon), duration=animation['pre_middle_duration'])
         pyautogui.click()
 
-        for i in range(0,15):
+        for i in range(0,100):
             # -----------------------------------------------------------------------
             # ---------------------   choose subcategory ----------------------------
             # -----------------------------------------------------------------------
@@ -95,14 +97,13 @@ class CheckSlots:
             icon = pyautogui.locateOnScreen('Images\\choose_subcategory.PNG', 5, grayscale=True, confidence=0.8)
             time.sleep(animation['fast_duration'])
             pyautogui.moveTo(*pyautogui.center(icon), duration=animation['slow_duration'])
-            pyautogui.move(50, 50, duration=animation['middle_duration'])
+            pyautogui.move(random.randint(-50, 50), 50, duration=animation['middle_duration'])
             pyautogui.click()
 
             image = 'Images\\subcategory_visa_c.PNG'
             # change subcategory to other visits
             if i % 2 == 1:
                 image = 'Images\\subcategory_visa_other_c.PNG'
-                time.sleep(animation['long_sleep'])
 
             # pick subcategory
             time.sleep(animation['pre_middle_duration'])
@@ -110,6 +111,7 @@ class CheckSlots:
             time.sleep(animation['fast_duration'])
             pyautogui.moveTo(*pyautogui.center(icon), duration=animation['slow_duration'])
             pyautogui.click()
+            print(f"{i} - iteration")
 
             # -----------------------------------------------------------------------
             # ---------------------   check if warning here -------------------------
@@ -126,5 +128,5 @@ class CheckSlots:
                     time.sleep(0.2)
                 pyautogui.alert(text='AVAILABLE DATES!!!', title='SUCCESS', button='OK')
 
-
+            time.sleep(random.randint(animation['short_sleep'],animation['XL_sleep']))
 
