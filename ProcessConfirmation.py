fc6742b4e18ece61a235b9edd5c5d1f2dd828c2f
Alex <blr.ymka@tut.by>
2024-04-29 11:40:04 +0300

    Added process confirmator

---
+++ ProcessConfirmation.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import random
+import winsound
+from Settings.Options import *
+from Settings.Credentials import credentials
+from Logger import Logger
+
+class CheckSlots:
+
+    def __init__(self):
+        self.logger = Logger()
+        self.title = 'Which step?'
+        self.text = ' BEGIN - full program\n LOGIN - starts from login menu\n CHECK_SLOTS - only change selected visa type'
+
+    def process(self, title=None, text=None):
+        code = pyautogui.confirm(
+            text=text or self.text,
+            title=title or self.title,
+            buttons=['begin', 'login', "check_slots", "pause", "sleep"]
+        )
+        return code
+
+
+
+
