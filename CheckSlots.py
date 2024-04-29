b93fd45d8b80f8c8eb33757014e857cf9173f3c4
Aleksandr <blr.ymka@tut.by>
2024-04-29 12:13:34 +0300

    check slots changes - animation changes - removed redundant try-catch

--- CheckSlots.py
+++ CheckSlots.py
@@ -14,7 +14,6 @@ class CheckSlots:
     def __init__(self):
         self.logger = Logger()
 
-
     def process(self):
         time.sleep(animation['short_sleep'])
         self.logger.log('Checking slots started')
@@ -42,22 +41,20 @@ class CheckSlots:
 
             self.logger.log(f"{i} - iteration completed...")
             print(f"{i} - iteration")
-            try:
-                arr[i].process()
-            except:
-                pyautogui.alert(text='CHECK SLOT FAILED', title='FAIL', button='OK')
+
+            arr[i].process()
+
             # -----------------------------------------------------------------------
             # ---------------------   check if warning here -------------------------
             # -----------------------------------------------------------------------
-            pyautogui.sleep(animation['fast_duration'])
+            pyautogui.sleep(animation['slow_duration'])
             pyautogui.scroll(-1000)
             try:
                 time.sleep(animation['middle_duration'])
                 pyautogui.locateOnScreen('Images\\sorry_notice.PNG', 5, grayscale=True, confidence=0.8)
-                time.sleep(animation['middle_duration'])
+                time.sleep(animation['fast_duration'])
                 # pyautogui.moveTo(*pyautogui.center(icon), duration=animation['slow_duration'])
             except:
-                print('SORRY NOT FOUND, SO SLOTS AVAILABLE')
                 for i in range(0, 3):
                     winsound.Beep(400, 800)
                     time.sleep(0.2)
@@ -66,7 +63,7 @@ class CheckSlots:
             for i in range(0, 30):
                 x, y = random.randint(200, 1800), random.randint(200, 1000)
                 pyautogui.moveTo(x, y, animation['middle_duration'], animation['animation'])
-            time.sleep(random.randint(15, 25))
+            time.sleep(random.randint(15, 20))
 
 
 
