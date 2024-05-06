e3f16a5ab1cf910c15c4007b716411d2839cb02a
Alex <blr.ymka@tut.by>
2024-05-06 22:58:00 +0300

    Added minsk shenghen d postal scenario

---
+++ CheckSlotScenaries/MinskShenghenDPostalScenario.py
@@ -0,0 +1,54 @@
+import pyautogui
+import time
+import random
+import winsound
+from Settings.Options import *
+from Settings.Credentials import credentials
+from Logger import Logger
+from CheckSlotScenaries.PickCenter import PickCenter
+from CheckSlotScenaries.PickCategory import PickCategory
+from CheckSlotScenaries.PickSubCategory import PickSubCategory
+from CheckSlotScenaries.PickBirthDate import PickBirthDate
+from CheckSlotScenaries.PickCitizenship import PickCitizenship
+
+
+class MinskShenghenDPostalScenario:
+
+    def __init__(self):
+        self.logger = Logger()
+
+    def process(self):
+        # -----------------------------------------------------------------------
+        # ---------------------   choose center   -------------------------------
+        # -----------------------------------------------------------------------
+        center = PickCenter()
+        center.process('Images\\minsk_center.PNG')
+
+        # -----------------------------------------------------------------------
+        # ---------------------   choose category -------------------------------
+        # -----------------------------------------------------------------------
+        category = PickCategory()
+        category.process('Images\\category_visa_d.PNG')
+
+        # -----------------------------------------------------------------------
+        # ---------------------   choose birthdate ------------------------------
+        # -----------------------------------------------------------------------
+        birthdate = PickBirthDate()
+        birthdate.process()
+
+        # -----------------------------------------------------------------------
+        # ---------------------   choose citizenship ----------------------------
+        # -----------------------------------------------------------------------
+        citizenship = PickCitizenship()
+        citizenship.process('Images\\citizenship_belarus.PNG')
+
+        # -----------------------------------------------------------------------
+        # ---------------------   choose subcategory ----------------------------
+        # -----------------------------------------------------------------------
+        subcategory = PickSubCategory()
+        subcategory.process('Images\\subcategory_visa_postal_d.PNG')
+
+
+
+
+
