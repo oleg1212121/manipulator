b783e7b755b187256f71337898527580e59fcde8
Alex <blr.ymka@tut.by>
2024-05-08 11:59:33 +0300

    Added time filler to start browser

--- StartBrowser.py
+++ StartBrowser.py
@@ -4,11 +4,14 @@ import random
 import winsound
 from Settings.Options import *
 from Logger import Logger
+from TimeFiller import TimeFiller
+
 
 class StartBrowser:
 
     def __init__(self):
         self.logger = Logger()
+        self.filler = TimeFiller()
 
     def process(self):
         time.sleep(animation['short_sleep'])
@@ -34,26 +37,9 @@ class StartBrowser:
             pyautogui.write(link)
             time.sleep(animation['pre_middle_duration'])
             pyautogui.press("enter")
-            for i in range(0, 5):
-                x, y = random.randint(200, 1800), random.randint(200, 1000)
-                pyautogui.moveTo(x, y, animation['pre_middle_duration'], animation['animation'])
+            self.filler.shake(4)
 
-        # wait for "is human" check
         time.sleep(animation['middle_sleep'])
 
-        # -----------------------------------------------------------------------
-        # ---------------------   IS HUMAN CHECK  -------------------------------
-        # -----------------------------------------------------------------------
-        # try:
-        #     icon = pyautogui.locateOnScreen(settings['login_not_robot'], 5, confidence=0.9, grayscale=True)
-        #     for i in range(0, 3):
-        #         winsound.Beep(200, 1000)
-        #         time.sleep(0.2)
-        #
-        #     pyautogui.moveTo(*coordinates["login_checkbox"], animation["middle_duration"], animation["animation"])
-        #     pyautogui.click()
-        #     time.sleep(animation['short_sleep'])
-        # except:
-        #     print('no robot check in vfs login')
 
 
