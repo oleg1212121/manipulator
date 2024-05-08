75d707fed535621f1bbbf11bb4732b8f6d6b5999
Alex <blr.ymka@tut.by>
2024-05-08 11:58:26 +0300

    Added time filler to check slots

--- CheckSlots.py
+++ CheckSlots.py
@@ -5,6 +5,7 @@ import winsound
 from Settings.Options import *
 from Settings.Credentials import credentials
 from Logger import Logger
+from TimeFiller import TimeFiller
 from CheckSlotScenaries.MinskShenghenCOtherScenario import MinskShenghenCOtherScenario
 from CheckSlotScenaries.MinskShenghenCVisitsScenario import MinskShenghenCVisitsScenario
 from CheckSlotScenaries.GrodnoShenghenCOtherScenario import GrodnoShenghenCOtherScenario
@@ -16,9 +17,10 @@ class CheckSlots:
 
     def __init__(self):
         self.logger = Logger()
+        self.filler = TimeFiller()
 
     def process(self):
-        time.sleep(animation['short_sleep'])
+        self.filler.shake(animation['short_sleep'])
         self.logger.log('Checking slots started')
         minsk_visits_c = MinskShenghenCVisitsScenario()
         minsk_other_c = MinskShenghenCOtherScenario()
@@ -49,23 +51,20 @@ class CheckSlots:
             # -----------------------------------------------------------------------
             # ---------------------   check if warning here -------------------------
             # -----------------------------------------------------------------------
-            pyautogui.sleep(animation['slow_duration'])
+            self.filler.shake(animation['slow_duration'])
             pyautogui.scroll(-1000)
             try:
-                time.sleep(animation['middle_duration'])
+                self.filler.shake(animation['middle_duration'])
                 pyautogui.locateOnScreen('Images\\sorry_notice.PNG', 5, grayscale=True, confidence=0.8)
-                time.sleep(animation['fast_duration'])
-                # pyautogui.moveTo(*pyautogui.center(icon), duration=animation['slow_duration'])
+                self.filler.shake(animation['fast_duration'])
             except:
                 for i in range(0, 3):
                     winsound.Beep(400, 800)
-                    time.sleep(0.2)
+                    self.filler.wait(0.2)
                 pyautogui.alert(text='AVAILABLE DATES!!!', title='SUCCESS', button='OK')
 
-            for i in range(0, 30):
-                x, y = random.randint(200, 1800), random.randint(200, 1000)
-                pyautogui.moveTo(x, y, animation['middle_duration'], animation['animation'])
-            time.sleep(random.randint(15, 25))
+            self.filler.activity(41)
+            self.filler.wait(random.randint(10, 20))
 
 
 
