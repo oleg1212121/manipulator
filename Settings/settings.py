3ec1dff743f1806039a6af1a6cd69ae7b4923e35
Alex <blr.ymka@tut.by>
2024-10-02 13:33:28 +0300

    Add main settings file

---
+++ Settings/settings.py
@@ -0,0 +1,21 @@
+from Settings.Proxies.settings import *
+from Settings.Elements.settings import *
+from Settings.Credentials.credentials import *
+
+paths = {
+    "httpbin_url": "http://httpbin.org/headers",
+    "vfs_categories_url": "https://lift-api.vfsglobal.com/master/visacategory/pol/blr/BAR/ru-RU",
+    "vfs_url": "https://visa.vfsglobal.com/blr/ru/pol/book-an-appointment",
+    "vfs_slots_url": "https://lift-api.vfsglobal.com/appointment/CheckIsSlotAvailable",
+    "bot_url": "https://bot.incolumitas.com/",
+    "vfs_log_url": "https://visa.vfsglobal.com/blr/ru/pol/login",
+    "myip_url": "https://myip.ru/",
+    "myipaddress_url": "https://whatismyipaddress.com/",
+    "nowsecure_url": "https://nowsecure.nl",
+    "mybrowser_url": "https://www.whatismybrowser.com/",
+    "cookie_accept": "button#onetrust-accept-btn-handler",
+    "images_folder": "Images\\",
+    "email_not_robot": "Images\\not_robot.PNG",
+    "login_not_robot": "Images\\ishuman.PNG",
+}
+cookie_accept = "button#onetrust-accept-btn-handler"
