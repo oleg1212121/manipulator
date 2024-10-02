fea7d03ddee5c36fb75fa66eab12d1999fef0ffc
Alex <blr.ymka@tut.by>
2024-10-02 13:44:02 +0300

    Change Cursor

--- Cursor.py
+++ Cursor.py
@@ -1,7 +1,13 @@
-import pyautogui
 import time
-import winsound
-from Settings.Options import *
+from Settings.Elements.settings import *
+from Manipulations.Simulations.TimeFiller import TimeFiller
+
+
+filler = TimeFiller()
+filler.wait(6.1)
+filler.shake(3.7)
+# filler.activity(5.4)
+
 
 
 # 820,770 - obywatelstwo
