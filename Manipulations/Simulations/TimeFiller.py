511d8bbeb0d08b479f1d130e7c863b797a76654d
Alex <blr.ymka@tut.by>
2024-10-02 13:19:41 +0300

    - Move TimeFiller

--- Manipulations/Simulations/TimeFiller.py
+++ Manipulations/Simulations/TimeFiller.py
@@ -1,7 +1,6 @@
-import pyautogui
 import time
 import random
-from Settings.Options import *
+from Settings.settings import *
 
 
 class TimeFiller:
@@ -10,24 +9,30 @@ class TimeFiller:
         pass
 
     def wait(self, seconds):
+        if seconds < 0.5:
+            seconds = 0.5
         time.sleep(seconds)
 
     def activity(self, seconds):
+        if seconds < 1:
+            seconds = 1
         position = pyautogui.position()
         dur = 1
         shakes = int(seconds // dur)
         for i in range(0, shakes - 1):
             x, y = random.randint(200, 1700), random.randint(200, 900)
-            pyautogui.moveTo(x, y, dur, animation['animation'])
-        pyautogui.moveTo(*position, dur, animation['animation'])
+            pyautogui.moveTo(x, y, dur, elements['animation']['animation'])
+        pyautogui.moveTo(*position, dur, elements['animation']['animation'])
         self.wait(seconds - (shakes * dur))
 
     def shake(self, seconds):
+        if seconds < 1:
+            seconds = 1
         position = pyautogui.position()
         dur = 0.2
         shakes = int(seconds // dur)
         for i in range(0, shakes-1):
             x, y = random.randint(position[0], position[0]+100), random.randint(position[1], position[1]+100)
-            pyautogui.moveTo(x, y, dur, animation['animation'])
-        pyautogui.moveTo(*position, dur, animation['animation'])
+            pyautogui.moveTo(x, y, dur, elements['animation']['animation'])
+        pyautogui.moveTo(*position, dur, elements['animation']['animation'])
         self.wait(seconds - (shakes * dur))
