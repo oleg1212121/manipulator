fd0810f34bb652abeb9072b5bc6f702392c744f5
Alex <blr.ymka@tut.by>
2024-10-02 13:41:27 +0300

    Change Logger

--- Manipulations/Logger/Logger.py
+++ Manipulations/Logger/Logger.py
@@ -1,13 +1,15 @@
 from datetime import datetime
+import os
 
 
 class Logger:
 
     def __init__(self):
+        self.path = os.path.dirname(__file__)
         pass
 
     def log(self, message):
-        with open("log.txt", "a") as f:
+        with open(f"{self.path}/log.txt", "a") as f:
             f.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "----------\n")
             f.write(str(message) + "\n")
 
